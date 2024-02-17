0506446fde49be4c662b433049e4654160b57bce
Mdshakilhossain141711 <freelancershakil141711@gmail.com>
2024-02-18 02:30:39 +0600

    vul

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-// alert("helo")
+let scrolBtn = document.getElementById("scrolBtn");
+
+
+scrolBtn.addEventListener("click", function () {
+    let scrol = document.getElementById("scrol");
+    scrol.scrollIntoView({
+        behavior: "smooth"
+    });
+});
